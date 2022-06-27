a5b4942047fe31d1d6574a234bfaff6b1067b8eb
Andrew Carolan <ajdcarolan+github@gmail.com>
2022-06-27 12:45:37 -0400

    Add catch-all route.

--- server/src/app.js
+++ server/src/app.js
@@ -14,6 +14,12 @@ const quotes = require("./routes/quotes");
 app.use(express.static(path.join(__dirname, "../../client/build")));
 app.use("/api/quotes", quotes);
 
+app.get("*", (req, res) => {
+  res.sendFile("index.html", {
+    root: path.join(__dirname, "../../client/build"),
+  });
+});
+
 app.listen(port, () => {
   console.log(`Express listening on port ${port}`);
   console.log("static path: ", path.join(__dirname, "../../client/build"));
